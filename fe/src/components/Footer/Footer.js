5e7c06faf939d3783b7a1c819ff0c7178f70613b
ducthvgcd19794 <ducthvgcd19794@fpt.edu.vn>
2023-04-01 22:46:42 +0700

    Update footer

---
+++ fe/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        CODE BY FPT
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
